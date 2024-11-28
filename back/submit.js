47346daa5af78b61823487ac6a1bf17c0f218bbd
MikuuBluee <shinzui079@gmail.com>
2024-11-28 07:35:59 +0700

    back

---
+++ back/submit.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+const dataManga = require('./manga')
+
+const formTambahManga = document.getElementById('formTambahManga');
+if (formTambahManga) {
+    formTambahManga.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        console.log('form submitted')
+
+        const Title = document.getElementById('floatingTitle').value;
+        const Chapter = document.getElementById('floatingChapter').value;
+        const Status = document.getElementById('floatingSelect1').value;
+        const ReleaseDate = document.getElementById('floatingReleaseDate').value;
+        const Sinopsis = document.getElementById('floatingTextarea2').value;
+        const MangaWriter = document.getElementById('floatingMangaWriter').value;
+        const ChapterPerEpisode = document.getElementById('floatingChapter-eps').value;
+        const Duration = document.getElementById('floatingDuration').value;
+        const Genre = document.getElementById('floatingSelect2').value;
+        
+        try {
+            const response = await axios.post('http://0.0.0.0:3000/manga/create', {
+                Title,
+                Chapter,
+                Status,
+                ReleaseDate,
+                Sinopsis,
+                MangaWriter,
+                ChapterPerEpisode,
+                Duration,
+                Genre,
+            });
+            console.log(response)
+
+            if(response.data.status === 'success'){
+                alert('Manga Berhasil Ditambahkan');
+                formTambahManga.reset();
+                window.location.href = 'Table.html'
+            } else{
+                alert('Manga Gagal Ditambahkan');
+            }
+        } catch (error) {
+            console.error('error add manga :', error);
+            alert('Terjadi Kesalahan Saat Menambahkan Manga');
+        }
+    });
+}
+
+const fetchManga = async () => {
+    try {
+    const response = await axios.get('http://0.0.0.0:3000/manga');
+    const mangaData = response.data.data.dataManga;
+
+    const tableBody = document.getElementById('table');
+    tableBody.innerHTML = '';
+
+    mangaData.forEach((manga) => {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+                <td>${manga.Title}</td>
+                <td>${manga.Chapter}</td>
+                <td>${manga.Status}</td>
+                <td>${manga.Genre}</td>
+                <td>${manga.ReleaseDate}</td>
+                <td>${manga.MangaWriter}</td>
+                <td>
+                    <button class="btn btn-warning btn-sm" onclick="editManga('${manga.id}')">Edit</button>
+                    <button class="btn btn-danger btn-sm" onclick="deleteManga('${manga.id}')">Delete</button>
+                </td>
+            `;
+            tableBody.appendChild(row);
+    });
+    } catch (error){
+        console.error('error fetch manga :', error);
+    }
+};
+
+window.onload = fetchManga;
