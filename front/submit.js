301c476c8159af377ee81721ed2188592b251a0f
MikuuBluee <shinzui079@gmail.com>
2024-11-28 14:20:38 +0700

    back

--- front/submit.js
+++ front/submit.js
@@ -10,13 +10,13 @@ if (formTambahManga) {
 
         const Title = document.getElementById('floatingTitle').value;
         const Chapter = document.getElementById('floatingChapter').value;
-        const Status = document.getElementById('floatingSelect1').value;
+        const Status = document.getElementById('floatingStatus').value;
         const ReleaseDate = document.getElementById('floatingReleaseDate').value;
-        const Sinopsis = document.getElementById('floatingTextarea2').value;
+        const Sinopsis = document.getElementById('floatingSinopsis').value;
         const MangaWriter = document.getElementById('floatingMangaWriter').value;
         const ChapterPerEpisode = document.getElementById('floatingChapter-eps').value;
         const Duration = document.getElementById('floatingDuration').value;
-        const Genre = document.getElementById('floatingSelect2').value;
+        const Genre = document.getElementById('floatingGenre').value;
         
         try {
             const response = await axios.post('http://localhost:3000/manga/create', {
@@ -52,39 +52,39 @@ const fetchManga = async () => {
     const mangaData = response.data.data.dataManga;
 
     const tableBody = document.getElementById('table');
-    tableBody.innerHTML = '';
+    tableBody.innerHTML = '<tr><th scope="col">Id</th><th scope="col">Title</th><th scope="col">Chapter</th><th scope="col">Status</th><th scope="col">Release Date</th><th scope="col">Sinopsis</th><th scope="col">Manga Writer</th><th scope="col">Chapter Per Episode</th><th scope="col">Duration</th><th scope="col">Genre</th><th scope="col">Edit/Delete</th></tr>';
 
     mangaData.forEach((manga) => {
-        const row = document.createElement('tr');
-
-        row.innerHTML = `<th scope="col">Id</th>
-                      <th scope="col">Title</th>
-                      <th scope="col">Chapter</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Genre</th>
-                      <th scope="col">Durasi</th>
-                      <th scope="col">Chapter per Episode</th>
-                      <th scope="col">Sinopsis</th>
-                      <th scope="col">Action</th>`
-
-        row.innerHTML = `
-                <td>${manga.Title}</td>
-                <td>${manga.Chapter}</td>
-                <td>${manga.Status}</td>
-                <td>${manga.Genre}</td>
-                <td>${manga.ReleaseDate}</td>
-                <td>${manga.MangaWriter}</td>
-                <td>
-                    <button class="btn btn-outline-warning btn" onclick="editManga('${manga.id}')">Edit</button>
-                    <button class="btn btn-outline-danger btn" onclick="deleteManga('${manga.id}')">Delete</button>
-                </td>
-            `;
-            
-            tableBody.appendChild(row);
+        tableBody.innerHTML += `<tr>
+                      <th scope="row">${manga.id}</th>
+                      <td>${manga.Title}</td>
+                      <td>${manga.Chapter}</td>
+                      <td>${manga.Status}</td>
+                      <td>${manga.ReleaseDate}</td>
+                      <td>${manga.Sinopsis}</td>
+                      <td>${manga.MangaWriter}</td>
+                      <td>${manga.ChapterPerEpisode}</td>
+                      <td>${manga.Duration}</td>
+                      <td>${manga.Genre}</td>
+                      <td><button type="submit" class="btn btn-outline-warning">Edit</button><button="submit" class="btn btn-outline-danger">Delete</button></td>
+                    </tr>`;
     });
     } catch (error){
         console.error('error fetch manga :', error);
     }
 };
 
-// window.onload = fetchManga;
+const deleteManga = async (id) => {
+    try {
+        const response = await axios.delete(`http://localhost:3000/manga/${id}`);
+
+        if(response.data.status === 'success') {
+            alert('Manga berhasil dihapus');
+            window.location.reload();
+        } else (
+            alert('Manga gagal dihapus')
+        )
+    } catch (error){
+        console.error('error Deleting manga:', error)
+    }
+};
