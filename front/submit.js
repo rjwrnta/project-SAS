e26a164e346934cb90a0498a61d6aeef5b80066f
MikuuBluee <shinzui079@gmail.com>
2024-11-28 08:45:36 +0700

    back

--- front/submit.js
+++ front/submit.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
-const dataManga = require('./manga')
+// import axios from 'axios';
+// const dataManga = require('../back/manga')
 
 const formTambahManga = document.getElementById('formTambahManga');
+
 if (formTambahManga) {
     formTambahManga.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -18,7 +19,7 @@ if (formTambahManga) {
         const Genre = document.getElementById('floatingSelect2').value;
         
         try {
-            const response = await axios.post('http://0.0.0.0:3000/manga/create', {
+            const response = await axios.post('http://localhost:3000/manga/create', {
                 Title,
                 Chapter,
                 Status,
@@ -33,8 +34,8 @@ if (formTambahManga) {
 
             if(response.data.status === 'success'){
                 alert('Manga Berhasil Ditambahkan');
-                formTambahManga.reset();
-                window.location.href = 'Table.html'
+                 formTambahManga.reset();
+                 window.location.href = 'Table.html'
             } else{
                 alert('Manga Gagal Ditambahkan');
             }
@@ -47,7 +48,7 @@ if (formTambahManga) {
 
 const fetchManga = async () => {
     try {
-    const response = await axios.get('http://0.0.0.0:3000/manga');
+    const response = await axios.get('http://localhost:3000/manga');
     const mangaData = response.data.data.dataManga;
 
     const tableBody = document.getElementById('table');
@@ -55,6 +56,16 @@ const fetchManga = async () => {
 
     mangaData.forEach((manga) => {
         const row = document.createElement('tr');
+
+        row.innerHTML = `<th scope="col">Id</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Chapter</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Genre</th>
+                      <th scope="col">Durasi</th>
+                      <th scope="col">Chapter per Episode</th>
+                      <th scope="col">Sinopsis</th>
+                      <th scope="col">Action</th>`
 
         row.innerHTML = `
                 <td>${manga.Title}</td>
@@ -64,10 +75,11 @@ const fetchManga = async () => {
                 <td>${manga.ReleaseDate}</td>
                 <td>${manga.MangaWriter}</td>
                 <td>
-                    <button class="btn btn-warning btn-sm" onclick="editManga('${manga.id}')">Edit</button>
-                    <button class="btn btn-danger btn-sm" onclick="deleteManga('${manga.id}')">Delete</button>
+                    <button class="btn btn-outline-warning btn" onclick="editManga('${manga.id}')">Edit</button>
+                    <button class="btn btn-outline-danger btn" onclick="deleteManga('${manga.id}')">Delete</button>
                 </td>
             `;
+            
             tableBody.appendChild(row);
     });
     } catch (error){
@@ -75,4 +87,4 @@ const fetchManga = async () => {
     }
 };
 
-window.onload = fetchManga;
+// window.onload = fetchManga;
